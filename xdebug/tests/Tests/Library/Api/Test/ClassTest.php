412bb9e5a0f9e4e6b75361a7ec4f59431b527940
Jorg Sowa <jorg.sowa@gmail.com>
2024-02-26 00:10:31 +0100

    xdebug reproducer
    Revert "xdebug reproducer"
    
    This reverts commit 47b93b77cd13efa24d7ff076c2ac88eebe3dc75a.
    
    add xdebug

---
+++ xdebug/tests/Tests/Library/Api/Test/ClassTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Library\Api\Test;
+
+
+use PHPUnit\Framework\TestCase;
+
+final class ClassTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        $this->setUp();
+    }
+
+
+    public function testSomething():void
+    {
+        self::assertTrue(true);
+    }
+}
